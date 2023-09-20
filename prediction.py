1b3369f4845b20e4fa85a3deacb57ed10f92f393
JasonP2711 <cthang2711@gmail.com>
2023-09-20 21:37:58 +0700

    up

--- prediction.py
+++ prediction.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO('yolov8n-seg.pt')  # load an official model
-model = YOLO('best.pt')  # load a custom model
+model = YOLO(r'.\NDVat\runs\segment\train\weights\best.pt')  # load a custom model
 
 # Predict with the model
-results = model('20230919_104710_jpg.rf.82ed36d6e0d9e6512348e114d1a0a375.jpg', save = True)  # predict on an image
+results = model(r'.\NDVat\train\images\20230919_104716_jpg.rf.747a0be5e965e191970e6a6c41c3ae68.jpg', save = True)  # predict on an image
 #picture
 # results = model.predict(source, save=True, imgsz=640, conf=0.5)
 
