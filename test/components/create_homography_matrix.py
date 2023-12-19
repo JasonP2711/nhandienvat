10a75e7b4b28aff27447a7e3b8668839bf0d8d58
JasonP2711 <cthang2711@gmail.com>
2023-12-20 06:44:44 +0700

    update convert

--- test/components/create_homography_matrix.py
+++ test/components/create_homography_matrix.py
@@ -6,6 +6,17 @@ def create_homography(point_pixel, point_robot,calib_path):
     point_pixel_homography = np.hstack((point_pixel,np.ones((point_pixel.shape[0],1))))
     transformation_matrix, _ = np.linalg.lstsq(point_pixel_homography, point_robot, rcond=None)[:2]
     homography_matrix = transformation_matrix.reshape(3, 3)
+    #c2 sử dụng cv2:
+    # Điểm tương ứng trên hình ảnh nguồn và đích
+        # pts_src = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+        # pts_dst = np.array([[u1, v1], [u2, v2], [u3, v3], [u4, v4]])
+
+        # # Ước lượng ma trận homography bằng RANSAC
+        # homography_matrix, _ = cv2.findHomography(pts_src, pts_dst, cv2.RANSAC, 5.0)
+
+        # # In ma trận homography
+        # print("Homography Matrix:")
+        # print(H)
     if not os.path.exists(calib_path):
             os.makedirs(calib_path)
             
